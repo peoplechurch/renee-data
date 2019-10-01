1d398e733f9f0794a08c5d6bd2a909f9acb4aff7
Logan Tran <loganmartintran@gmail.com>
2019-10-01 00:43:27 -0500

    add new creation team and growth track team to account attribute

--- data.go
+++ data.go
@@ -34,15 +34,17 @@ type Member struct {
 
 // this is how an tenant account is stored to determine what org/campus renee belongs to
 type Account struct {
-	Campus          string `json:"Campus"`
-	ClerkNumber     string `json:"ClerkNumber"`
-	GiveTrigger     string `json:"GiveTrigger"`
-	HomeTrigger     string `json:"HomeTrigger"`
-	NCTrigger       string `json:"NCTrigger"`
-	Organization    string `json:"Organization"`
-	SlackBotToken   string `json:"SlackBotToken"`
-	SlackOAuthToken string `json:"SlackOAuthToken"`
-	UUID            string `json:"UUID"`
+	Campus           string `json:"Campus"`
+	ClerkNumber      string `json:"ClerkNumber"`
+	GiveTrigger      string `json:"GiveTrigger"`
+	HomeTrigger      string `json:"HomeTrigger"`
+	NCTrigger        string `json:"NCTrigger"`
+	Organization     string `json:"Organization"`
+	SlackBotToken    string `json:"SlackBotToken"`
+	SlackOAuthToken  string `json:"SlackOAuthToken"`
+	UUID             string `json:"UUID"`
+	NewCreationsTeam string `json:"NewCreationsTeam"`
+	GrowthTrackTeam  string `json:"GrowthTrackTeam"`
 }
 
 type SpiritualGiftsData struct {
